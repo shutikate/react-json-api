36ec4f77e169b5f5f17d145e47bc0e27cbf3e2e3
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-04-04 13:07:07 +0200

    feat: add api

---
+++ index.js
@@ -0,0 +1,14 @@
+import jsonServer from 'json-server';
+import cors from 'cors';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = 3000;
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+server.listen(port);
+
+
