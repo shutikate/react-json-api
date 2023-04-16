cf33bfe35ddc08c9f9263b9b4fd9cbe365bff6dc
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2023-04-16 22:44:10 +0200

    fix: cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = 3000;
 
-server.use(cors());
+server.use(cors({origin: '*'}));
 server.use(middlewares);
 server.use(router);
 server.listen(port);
